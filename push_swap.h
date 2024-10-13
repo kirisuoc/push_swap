605cef61d34d18b7277670309c8259394ff31d12
kirisuoc <erikcousillas@icloud.com>
2024-10-13 22:49:06 +0100

    Add file .h

---
+++ push_swap.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erikcousillas <erikcousillas@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/12 17:49:23 by erikcousill       #+#    #+#             */
+/*   Updated: 2024/10/13 23:30:40 by erikcousill      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+typedef struct s_stack
+{
+	int	*data;	// Array de enteros que almacena los elementos del stack
+	int	top;	// Índice del último elemento del stack (similar a puntero)
+}		t_stack;
+
+void	sa(t_stack *a);
+void	sb(t_stack *b);
+void	ss(t_stack *a, t_stack *b);
+void	pa(t_stack *a, t_stack *b);
+void	pb(t_stack *b, t_stack *a);
+void	ra(t_stack *a);
+void	rb(t_stack *b);
+void	rr(t_stack *a, t_stack *b);
+void	rra(t_stack *a);
+void	rrb(t_stack *b);
+void	rrr(t_stack *a, t_stack *b);
+
+
+
+#endif
