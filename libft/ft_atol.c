d782087d135029b33d338be20ea8d7affabaa68d
kirisuoc <erikcousillas@icloud.com>
2024-10-15 19:00:51 +0100

    Add function ft_atol to libft

---
+++ libft/ft_atol.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erikcousillas <erikcousillas@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/15 18:20:51 by erikcousill       #+#    #+#             */
+/*   Updated: 2024/10/15 18:24:31 by erikcousill      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long	ft_atol(const char *nptr)
+{
+	size_t		i;
+	long	number;
+	int		sign;
+
+	i = 0;
+	number = 0;
+	sign = 1;
+	while (nptr[i] == ' ' || nptr[i] == '\t' || nptr[i] == '\n'
+		|| nptr[i] == '\v' || nptr[i] == '\f' || nptr[i] == '\r')
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign *= -1;
+		i++;
+		if (nptr[i] == '-' || nptr[i] == '+')
+			return (0);
+	}
+	while (ft_isdigit(nptr[i]))
+	{
+		number = number * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (number * sign);
+}
