4564a89652deb04567e544fb7b4c49a25e4d5f26
kirisuoc <erikcousillas@icloud.com>
2024-10-15 14:18:05 +0100

    main.c with fill_stack with argument passed to the program

--- main.c
+++ main.c
@@ -6,15 +6,45 @@
 /*   By: erikcousillas <erikcousillas@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 18:06:44 by erikcousill       #+#    #+#             */
-/*   Updated: 2024/10/12 18:12:50 by erikcousill      ###   ########.fr       */
+/*   Updated: 2024/10/15 14:04:29 by erikcousill      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft/libft.h"
 #include "./libft/libftprintf.h"
+#include "push_swap.h"
 
-int main(void)
+void	fill_stack(t_stack *a, int argc, char **argv)
 {
-    ft_printf("Hola, Mundo.\n");
-    return (0);
+	int i;
+
+	i = 1;
+	a->top = argc - 2;
+	while (i < argc)
+	{
+		a->data[i - 1] = ft_atoi(argv[argc - i]);
+		i++;
+	}
 }
+
+int main(int argc, char **argv)
+{
+	t_stack a;
+
+	a.data = malloc((argc - 1) * sizeof(int));
+	if (!a.data)
+	{
+		ft_printf("Error\n");
+		return (1);
+	}
+	fill_stack(&a, argc, argv);
+
+	// Imprimir el contenido del stack desde el top hasta el bottom
+	for (int j = a.top; j >= 0; j--) { // Cambiado para imprimir desde el top
+		ft_printf("%d\n", a.data[j]);
+	}
+
+	free(a.data);
+	return (0);
+}
+
