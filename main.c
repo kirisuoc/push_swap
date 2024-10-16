cb3d66dbd03c4a965b4ac322fea94535f02261f3
kirisuoc <erikcousillas@icloud.com>
2024-10-16 17:48:07 +0100

    Sort lists up to 5

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: erikcousillas <erikcousillas@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 18:06:44 by erikcousill       #+#    #+#             */
-/*   Updated: 2024/10/15 23:41:43 by erikcousill      ###   ########.fr       */
+/*   Updated: 2024/10/16 17:45:53 by erikcousill      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,12 @@ static int	is_sorted(t_stack *a)
 {
 	int	i;
 
+	if (a->top < 1)
+		return (1);
 	i = 0;
 	while (i < a->top)
 	{
-		if (a->data[a->top - i] > a->data[a->top - i - 1])
+		if (a->data[i] < a->data[i + 1])
 			return (0);
 		i++;
 	}
@@ -42,6 +44,57 @@ void	fill_stack(t_stack *a, int argc, char **argv)
 	}
 }
 
+void	short_list(t_stack *a)
+{
+	while (!is_sorted(a))
+	{
+		if (a->data[a->top] > a->data[a->top - 1])
+			sa(a, 1);
+		else
+			rra(a, 1);
+	}
+}
+
+void	list_upto_5(t_stack *a, t_stack *b)
+{
+	while (a->top > 2)
+		pb(b, a);
+	short_list(a);
+	while (b->top > -1)
+	{
+		if (b->data[b->top] < a->data[a->top])
+			pa(a, b);
+		else if (b->data[b->top] > a->data[0])
+		{
+			pa(a, b);
+			ra(a, 1);
+		}
+		else
+		{
+			while (b->data[b->top] > a->data[a->top] || b->data[b->top] < a->data[0])
+				ra(a, 1);
+			pa(a, b);
+
+		}
+		while (!is_sorted(a))
+			rra(a, 1);
+	}
+}
+
+
+void	sort_list(t_stack *a, t_stack *b)
+{
+	if (a->top + 1 == 3)
+	{
+		short_list(a);
+	}
+	else if (a->top + 1 <= 5)
+	{
+		list_upto_5(a, b);
+	}
+}
+
+
 int main(int argc, char **argv)
 {
 	t_stack a;
@@ -51,60 +104,32 @@ int main(int argc, char **argv)
 		return (1);
 	if (wrong_input(argc, argv))
 		return (1);
-
 	a.data = ft_calloc((argc - 1), sizeof(int));
 	b.data = ft_calloc((argc - 1), sizeof(int));
 	if (!a.data || !b.data)
 		return (1);
-
 	fill_stack(&a, argc, argv);
-
 	if (is_sorted(&a))
 	{
 		free(a.data);
 		free(b.data);
 		return (0);
 	}
-
 	b.top = -1;
 
-/* 	while (!is_sorted(&a))
-	{
+	sort_list(&a, &b);
 
-	}
- */
+  							// Imprimir el contenido del stack A desde el top hasta el bottom
+							ft_printf("\n");
 
-	sa(&a, 1);
-	pb(&b, &a);
-	pb(&b, &a);
-	pb(&b, &a);
-	sa(&a, 1);
-	pa(&a, &b);
-	pa(&a, &b);
-	pa(&a, &b);
-
-/*	write(1, "sa\n", 3);
-	write(1, "pb\n", 3);
-	write(1, "pb\n", 3);
-	write(1, "pb\n", 3);
-	write(1, "sa\n", 3);
-	write(1, "pa\n", 3);
-	write(1, "pa\n", 3);
-	write(1, "pa\n", 3);
- */
-
-
-	// print_instructions??
-
- 							// Imprimir el contenido del stack A desde el top hasta el bottom
-/* 							for (int j = a.top; j >= 0; j--) { // Cambiado para imprimir desde el top
+ 							for (int j = a.top; j >= 0; j--) {
 								ft_printf("%d ", a.data[j]);
 								ft_printf("%d", b.data[j]);
 								ft_printf("\n");
 							}
 							ft_printf("_ _\na b\n");
 
-							ft_printf("%d\n", b.top); */
+							ft_printf("%d\n", b.top);
 	free(a.data);
 	free(b.data);
 	return (0);
