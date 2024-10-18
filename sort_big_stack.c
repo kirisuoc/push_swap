4539ffdbc073da09dc2c94843d27cb90053361f1
kirisuoc <erikcousillas@icloud.com>
2024-10-18 21:23:31 +0100

    Last version working

--- sort_big_stack.c
+++ sort_big_stack.c
@@ -6,7 +6,7 @@
 /*   By: erikcousillas <erikcousillas@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 21:29:05 by erikcousill       #+#    #+#             */
-/*   Updated: 2024/10/18 18:53:16 by erikcousill      ###   ########.fr       */
+/*   Updated: 2024/10/18 21:16:55 by erikcousill      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,45 @@
 static int	get_smallest_number(t_stack *a)
 {
 	int	i;
-	int	min_number;
+	int	smallest;
 
 	i = a->top;
-	min_number = a->data[0];
-	while (i >= 0)
+	smallest = a->data[0];
+	while (i > 0)
 	{
-		if (a->data[i] < min_number)
-			min_number = a->data[i];
+		if (a->data[i] < smallest)
+			smallest = a->data[i];
 		i--;
 	}
-	return (min_number);
+	return (smallest);
 }
 
-static int	get_distance_ra(t_stack *a)
+static int	get_largest_number(t_stack *b)
+{
+	int i;
+	int largest;
+
+	i = b->top;
+	largest = b->data[0];
+	while (i > 0)
+	{
+		if (b->data[i] > largest)
+			largest = b->data[i];
+		i--;
+	}
+	return (largest);
+}
+
+static int	get_distance_r(t_stack *a, int	number)
 {
 	int	i;
-	int	min_number;
 	int	distance_ra;
 
-	min_number = get_smallest_number(a);
 	distance_ra = 0;
 	i = a->top;
 	while (i >= 0)
 	{
-		if (a->data[i] == min_number)
+		if (a->data[i] == number)
 			break ;
 		distance_ra++;
 		i--;
@@ -47,18 +61,16 @@ static int	get_distance_ra(t_stack *a)
 	return (distance_ra);
 }
 
-static int	get_distance_rra(t_stack *a)
+static int	get_distance_rr(t_stack *a, int number)
 {
 	int	i;
-	int	min_number;
 	int	distance_rra;
 
-	min_number = get_smallest_number(a);
 	distance_rra = 0;
 	i = 0;
 	while (i <= a->top)
 	{
-		if (a->data[i] == min_number)
+		if (a->data[i] == number)
 		{
 			distance_rra = i + 1;
 			break ;
@@ -68,28 +80,61 @@ static int	get_distance_rra(t_stack *a)
 	return (distance_rra);
 }
 
+
+static void	back_to_a(t_stack *a, t_stack *b)
+{
+	int	distance_rb;
+	int	distance_rrb;
+	int	max_number;
+
+	while (b->top > -1)
+	{
+		max_number = get_largest_number(b);
+		distance_rb = get_distance_r(b, max_number);
+		distance_rrb = get_distance_rr(b, max_number);
+		if (distance_rb <= distance_rrb)
+		{
+			while (distance_rb-- > 0)
+				rb(b, 1);
+		}
+		else
+		{
+			while (distance_rrb-- > 0)
+				rrb(b, 1);
+		}
+		pa(a, b);
+	}
+}
+
 void	big_stack(t_stack *a, t_stack *b)
 {
 	int	distance_ra;
 	int	distance_rra;
+	int	min_number;
 
-	while (a->top > 2)
+	while (a->top > -1)
 	{
-		distance_ra = get_distance_ra(a);
-		distance_rra = get_distance_rra(a);
+		min_number = get_smallest_number(a);
+		distance_ra = get_distance_r(a, min_number);
+		distance_rra = get_distance_rr(a, min_number);
 		if (distance_ra <= distance_rra)
 		{
-			while (distance_ra-- > 0)
+			while (distance_ra-- >= 0)
+			{
+				if (a->data[a->top] >= min_number - 15 && a->data[a->top] <= min_number + 15)
+					pb(b, a);
 				ra(a, 1);
+			}
 		}
 		else
 		{
-			while (distance_rra-- > 0)
+			while (distance_rra-- >= 0)
+			{
+				if (a->data[a->top] >= min_number - 15 && a->data[a->top] <= min_number + 15)
+					pb(b, a);
 				rra(a, 1);
+			}
 		}
-		pb(b, a);
 	}
-	short_list(a);
-	while (b->top > -1)
-		pa(a, b);
+	back_to_a(a, b);
 }
