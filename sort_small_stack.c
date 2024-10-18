24cc1ceea83a023de4255e8612a2a77470b6aaba
kirisuoc <erikcousillas@icloud.com>
2024-10-18 19:08:11 +0100

    Remove innecessary function

--- sort_small_stack.c
+++ sort_small_stack.c
@@ -6,7 +6,7 @@
 /*   By: erikcousillas <erikcousillas@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 21:25:04 by erikcousill       #+#    #+#             */
-/*   Updated: 2024/10/17 21:25:47 by erikcousill      ###   ########.fr       */
+/*   Updated: 2024/10/18 17:44:27 by erikcousill      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,32 +19,6 @@ void	short_list(t_stack *a)
 		if (a->data[a->top] > a->data[a->top - 1])
 			sa(a, 1);
 		else
-			rra(a, 1);
-	}
-}
-
-void	up_to_5(t_stack *a, t_stack *b)
-{
-	while (a->top > 2)
-		pb(b, a);
-	short_list(a);
-	while (b->top > -1)
-	{
-		if (b->data[b->top] < a->data[a->top])
-			pa(a, b);
-		else if (b->data[b->top] > a->data[0])
-		{
-			pa(a, b);
-			ra(a, 1);
-		}
-		else
-		{
-			while (b->data[b->top] > a->data[a->top] || b->data[b->top] < a->data[0])
-				ra(a, 1);
-			pa(a, b);
-
-		}
-		while (!is_sorted(a))
 			rra(a, 1);
 	}
 }
