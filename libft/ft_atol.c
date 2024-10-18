9e40c9e8abd9b5afa4a545ce94eecb4d17e934b9
kirisuoc <erikcousillas@icloud.com>
2024-10-18 19:07:47 +0100

    Fix Norminette

--- libft/ft_atol.c
+++ libft/ft_atol.c
@@ -6,7 +6,7 @@
 /*   By: erikcousillas <erikcousillas@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 18:20:51 by erikcousill       #+#    #+#             */
-/*   Updated: 2024/10/15 18:24:31 by erikcousill      ###   ########.fr       */
+/*   Updated: 2024/10/18 18:50:03 by erikcousill      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 long	ft_atol(const char *nptr)
 {
 	size_t		i;
-	long	number;
-	int		sign;
+	long		number;
+	int			sign;
 
 	i = 0;
 	number = 0;
