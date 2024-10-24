da005414cdc802da3aa7015f80874a103cae1e2b
Erik Cousillas Fernandez <ecousill@c3r5s5.42urduliz.com>
2024-10-24 17:34:22 +0200

    ok

--- sort_big_stack.c
+++ sort_big_stack.c
@@ -6,7 +6,7 @@
 /*   By: ecousill <ecousill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 15:21:53 by ecousill          #+#    #+#             */
-/*   Updated: 2024/10/24 17:26:58 by ecousill         ###   ########.fr       */
+/*   Updated: 2024/10/24 17:33:27 by ecousill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,6 @@ static void	make_move(t_stack *a, t_stack *b, t_moves *next_move)
 {
 	int i;
 
-	//ft_printf(GREEN"next_move->index: %d\n", next_move->index);
-	//ft_printf("next_move->is_reverse_a: %d\n", next_move->is_reverse_a);
-	//ft_printf("next_move->distance_ra: %d\n", next_move->distance_ra);
-	//ft_printf("next_move->distance_rra: %d\n", next_move->distance_rra);
-	//ft_printf("next_move->is_reverse_b: %d\n"RESET, next_move->is_reverse_b);
 	if (!next_move->is_reverse_a)
 		while (next_move->distance_ra-- > 0)
 			ra(a, 1);
@@ -40,7 +35,7 @@ static void	make_move(t_stack *a, t_stack *b, t_moves *next_move)
 	{
 		if (a->data[a->top] > b->data[b->top] && a->data[a->top] < b->data[0])
 		{
-			pb(b, a); // Cuando 'b' ya está en el sitio correcto para introducir el número arriba
+			pb(b, a);
 			return ;
 		}
 		else if (!next_move->is_reverse_b)
@@ -60,39 +55,9 @@ static void	make_move(t_stack *a, t_stack *b, t_moves *next_move)
 			rrb(b, 1);
 		}
 	}
-	pb(b, a); // Cuando 'b' ya está en el sitio correcto para introducir el número arriba
+	pb(b, a);
 }
 
-int	get_position(t_stack *b, int max_number)
-{
-	int	max_pos;
-
-	max_pos = 0;
-	while (max_number != b->data[max_pos])
-		max_pos++;
-	return (max_pos);
-}
-
-void final_sort_b(t_stack *b, t_moves *next_move)
-{
-    int max_pos;
-
-	max_pos = get_position(b, next_move->max_number);
-
-    // Si el valor máximo está en la primera mitad del stack, es más eficiente usar 'rb'
-    if (max_pos >= b->top / 2)
-    {
-        // Rotar hacia abajo hasta que esté ordenado en orden descendente
-        while (!is_sorted_reverse(b))
-            rb(b, 1);
-    }
-    else
-    {
-        // Si el valor máximo está en la segunda mitad, rotar hacia arriba con 'rrb'
-        while (!is_sorted_reverse(b))
-            rrb(b, 1);
-    }
-}
 
 void	sort_big_stack(t_stack *a, t_stack *b)
 {
